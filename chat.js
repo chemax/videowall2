668193cd699cf8b62f03e10a9f05d0da2e810260
Maksim Chornopolsky <chemax.kr@gmail.com>
2017-05-18 15:05:27 +0700

    initial commit

---
+++ chat.js
@@ -0,0 +1,30 @@
+﻿var express = require('express'); // Подключаем express
+var app = express();
+var fs = require('fs'); // required for file serving
+var foldername = '/var/www/thumb/';
+var server = require('http').Server(app); // Подключаем http через app
+var io = require('socket.io')(server); // Подключаем socket.io и указываем на сервер
+var log4js = require('log4js'); // Подключаем наш логгер
+var logger = log4js.getLogger(); // Подключаем с модуля log4js сам логгер
+var port = 3000; // Можно любой другой порт
+
+logger.debug('Script has been started...'); // Логгируем.
+server.listen(port); // Теперь мы можем подключиться к нашему серверу через localhost:3000 при запущенном скрипте
+app.use(express.static(__dirname + '/public')); // Отправляет "статические" файлы из папки public при коннекте // __dirname - путь по которому лежит chat.js
+io.on('connection', function (socket) {
+ 	var name = 'U' + (socket.id).toString().substr(1,4); // Создаем никнейм нашему клиенту. В начале буква 'U' дальше берем 3 символа ID (сокета) после первого символа, и все это клеим с помощью '+'
+	
+	socket.broadcast.emit('newUser', name); // Отсылает событие 'newUser' всем подключенным, кроме текущего. На клиенте навешаем обработчик на 'newUser' (Отправляет клиентам событие о подключении нового юзера)
+	socket.emit('userName', name); // Отправляем текущему клиенту событие 'userName' с его ником (name) (Отправляем клиенту его юзернейм)
+	logger.info(name + ' connected to chat!'); // Логгирование
+	fs.watch(foldername, (eventType, filename) => {
+	  logger.info(filename + ' has been changed');
+	  socket.broadcast.emit('message', filename + " has been changed"); 
+		fs.readFile(foldername + filename, function(err, buf){
+			socket.emit('image', {filename: filename, image: true, buffer: buf.toString('base64') });
+			logger.info('image file is initialized');
+		});
+	});
+	
+	
+});
---
+++ public/main.js
@@ -0,0 +1,77 @@
+﻿var port = 3000; // Указываем порт на котором у на стоит сокет
+var socket = io.connect('http://192.168.22.239:' + port); // Тут мы объявляем "socket" (дальше мы будем с ним работать) и подключаемся сразу к серверу через порт
+
+socket.on('connect', function(){
+	console.log('connect establish!');
+    
+  });
+
+socket.on('userName', function(userName){ // Создаем прослушку 'userName' и принимаем переменную name в виде аргумента 'userName'
+console.log('You\'r username is => ' + userName); // Логгирование в консоль браузера
+$('textarea').val($('textarea').val() + 'You\'r username => ' + userName + '\n'); // Выводим в поле для текста оповещение для подключенного с его ником
+});
+
+socket.on('newUser', function(userName){ // Думаю тут понятно уже =)
+console.log('New user has been connected to chat | ' + userName); // Логгирование
+$('textarea').val($('textarea').val() + userName + ' connected!\n'); // Это событие было отправлено всем кроме только подключенного, по этому мы пишем другим юзерам в поле что 'подключен новый юзер' с его ником
+});
+socket.on('message', function(userName){ // Думаю тут понятно уже =)
+console.log(userName); // Логгирование
+$('textarea').val($('textarea').val() + userName + '\n' ); // Это событие было отправлено всем кроме только подключенного, по этому мы пишем другим юзерам в поле что 'подключен новый юзер' с его ником
+});
+/* socket.on("image", function(image, buffer) {
+     if(image)
+     {
+         console.log(" image: from client side");
+         // code to handle buffer like drawing with canvas** <--- is canvas drawing/library a requirement?  is there an alternative? another quick and dirty solution?
+        console.log(image);
+        // what can we do here to serve the image onto an img tag?
+     }
+
+ }); */
+
+ 
+ socket.on("image", function(info) {
+  if (info.image) {
+    var img = new Image();
+    img.src = 'data:image/jpeg;base64,' + info.buffer;
+	//$('#theDiv').append('<img id="theImg" src="'+img.src+'" />')
+	//$("#cam").attr("src","");
+	var element=document.getElementById(info.filename);
+	if(!element){
+		$('div#cameras').append('<div id="'+info.filename+'"><img src="'+img.src+'" id="'+info.filename+'" onerror="this.src=\'gold.gif\'"></div>');
+
+	} 
+	else {
+		$( "div#"+info.filename ).replaceWith( '<img src="'+img.src+'" id="'+info.filename+'" onerror="this.src=\'gold.gif\'">' );
+
+		//$('img#'+info.filename).attr("src","");
+		//$('img#'+info.filename).attr("src",img.src);
+		
+	}
+
+	//$( "div#" + info.filename ).replaceWith( '<img src="'+img.src+'">' );
+	//$("#cam").attr("src",img.src);
+	//delete img;
+    //ctx.drawImage(img, 0, 0);
+	//console.log(img.src);
+  }
+});
+ 
+/* $(function(){
+  var socket = io.connect('http://0.0.0.0:5001');
+
+  socket.on('connect', function(){
+    var delivery = new Delivery(socket);
+
+    delivery.on('receive.start',function(fileUID){
+      console.log('receiving a file!');
+    });
+
+    delivery.on('receive.success',function(file){
+      if (file.isImage()) {
+        $('img').attr('src', file.dataURL());
+      };
+    });
+  });
+}); */
